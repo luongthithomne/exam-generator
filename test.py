988ca8685f213dc9063f9c6dfbec08e192fa36c2
luongthithomne <thomlt.khmt@gmail.com>
2024-12-13 14:46:21 +0700

    sdf

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import re
 # Đường dẫn tới tệp Excel
 input_file = 'C:\DEANTN\genarate_question\datafn.xls'  # hoặc .xlsx
 output_file = 'C:\DEANTN\genarate_question\data.csv'
@@ -7,9 +7,15 @@ output_file = 'C:\DEANTN\genarate_question\data.csv'
 # Đọc tệp Excel
 data = pd.read_excel(input_file)
 
+
+
 # Xóa chuỗi lặp "Câu X: Câu Y"
 data['CAUHOI'] = data['CAUHOI'].apply(lambda x: re.sub(r"(Câu\s+\d+:)\s*(Câu\s+\d+:)?", "Câu ", x))
 
+# Làm sạch đáp án: Xóa lặp kiểu "A. A."
+data['CAUTRALOI'] = data['CAUTRALOI'].apply(lambda x: '~'.join(
+    re.sub(r"^[A-Z]\.\s*[A-Z]\.\s*", "", answer.strip()) for answer in x.split('~')
+))
 # Ghi tệp CSV
 data.to_csv(output_file, index=False, encoding='utf-8', sep=";")
 
